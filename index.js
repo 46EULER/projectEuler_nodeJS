60ee5affd0785066ea70c0b3808bbd15b2eefa6f
46EULER <forfirefly@foxmail.com>
2022-12-05 14:06:39 +0800

    add Question 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 function isFileExisted(path_way){
+/*
     return new Promise((resolve,reject) =>{
         fs.access(path_way, (err)=>{
             if (err){
@@ -10,26 +11,38 @@ function isFileExisted(path_way){
             }
         })
     })
+*/
+
+    return fs.promises.access(path_way, fs.constants.F_OK)
+             .then(() => true)
+             .catch(() => false)
+  
+
 };
+
+
+
+
 
 function padLeadingZeros(num, size) {
     var s = num+"";
     while (s.length < size) s = "0" + s;
     return s;
 };
+
 function Question(eulerQNum){
     var questionFileName ='./resultsOfEuler/Q'+padLeadingZeros(eulerQNum,4);
-    if (isFileExisted(questionFileName+'.js')){
-        var resultQ = require(questionFileName);
-        return "Answer: \n"+resultQ.answer +"\n\nDescription of question"+ eulerQNum + "\n"+resultQ.questionDescription+"\nhttps://projecteuler.net/problem="+eulerQNum;
-    
-    }else{
-        return "Question "+eulerQNum+" is not solved yet.";
-    }
-
-      
+      if (isFileExisted(questionFileName+'.js')){
+            var resultQ = require(questionFileName);
+            return "Answer: \n"+resultQ.answer +"\n\nDescription of question"+ eulerQNum + "\n"+resultQ.questionDescription+"\nhttps://projecteuler.net/problem="+eulerQNum;
+        
+        }else{
+            return "Question "+eulerQNum+" is not solved yet.";
+        }
+ 
     
 };
+
 function test(){
     return "got number:"
 };
